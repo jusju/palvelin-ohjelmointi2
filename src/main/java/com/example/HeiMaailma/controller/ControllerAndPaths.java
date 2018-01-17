e07d8e860fbb1e5dd7594650cb4ee4cabd6e9bde
Hallenberg Felix <a1700342@myy.haaga-helia.fi>
2018-01-17 11:28:58 +0200

    Ensimmäisen tunnin tekemiset

---
+++ src/main/java/com/example/HeiMaailma/controller/ControllerAndPaths.java
@@ -0,0 +1,17 @@
+package com.example.HeiMaailma.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class ControllerAndPaths {
+	@RequestMapping("/index")
+	public String index(){
+		return "This is the main page";
+}
+	@RequestMapping("/contact")
+	public String contact(){
+		return "This is the contact page";
+	}
+}
--- src/main/java/com/example/HeiMaailma/controller/HeiController.java
+++ src/main/java/com/example/HeiMaailma/controller/HeiController.java
@@ -2,13 +2,14 @@ package com.example.HeiMaailma.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
 public class HeiController {
-@RequestMapping("*")
-public String hei(){
-	return "Hei Maailma";
-}
+	@RequestMapping("/hello")
+	public String hei(@RequestParam(place = "location") String location, @RequestParam(name = "name") String name) {
+		return "Welcome to the " + location + " " + name;
+	}
 }
