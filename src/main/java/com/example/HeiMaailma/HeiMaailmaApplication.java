85b7e0ba21f2afaeb4be501b73123496ae59c3e2
Hallenberg Felix <a1700342@myy.haaga-helia.fi>
2018-01-17 10:41:58 +0200

    lisäsin tiedostoja

---
+++ src/main/java/com/example/HeiMaailma/HeiMaailmaApplication.java
@@ -0,0 +1,12 @@
+package com.example.HeiMaailma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeiMaailmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeiMaailmaApplication.class, args);
+	}
+}
