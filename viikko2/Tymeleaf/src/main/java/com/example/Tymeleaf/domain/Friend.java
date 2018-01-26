4447f1f2d3daad33a63e86f0ada9e54a31f56ff3
rottabonus <a1700342@myy.haaga-helia.fi>
2018-01-26 09:08:21 +0200

    viikon 2 harjoituksia 1-2

---
+++ viikko2/Tymeleaf/src/main/java/com/example/Tymeleaf/domain/Friend.java
@@ -0,0 +1,25 @@
+package com.example.Tymeleaf.domain;
+
+public class Friend {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Friend() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Friend(String name) {
+		super();
+		this.name = name;
+	}
+	
+	
+}
