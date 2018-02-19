0a4c264b1a244617f94898165f514a1a07f476ba
Felix Hallenberg <felix.hallenberg@myy.haaga-helia.fi>
2018-02-19 20:57:44 +0200

    BookStore: return all books as  JSON, Use path to get book id, added login, show authenticated username in view, implemented userdetailservice, crud repository for users picturegallery: added login

--- harjoitustyö/gallery/src/main/java/com/gallery/gallery/domain/PictureRepository.java
+++ harjoitustyö/gallery/src/main/java/com/gallery/gallery/domain/PictureRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PictureRepository extends CrudRepository<Picture, Long>{
 
+	 Picture findByName(String name);
 }
--- viikko3/Bookstore/src/main/java/palvelinohjelmointi/Bookstore/web/BookController.java
+++ viikko3/Bookstore/src/main/java/palvelinohjelmointi/Bookstore/web/BookController.java
@@ -1,11 +1,14 @@
 package palvelinohjelmointi.Bookstore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import palvelinohjelmointi.Bookstore.domain.Book;
 import palvelinohjelmointi.Bookstore.domain.BookRepository;
@@ -18,11 +21,21 @@ public class BookController {
 	@Autowired
 	private CategoryRepository crepository;
 	
+	@RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }	
+	
 	@RequestMapping(value="/booklist")
 	public String bookList(Model model) {
-		model.addAttribute("books", repository.findAll());
+		model.addAttribute("books", repository.findAll()); //
 		return "booklist";
 	}
+	
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> booklist() {	
+        return (List<Book>) repository.findAll();
+    }    
 	
 	@RequestMapping(value="/addbook")
 	public String addBook(Model model){
@@ -45,9 +58,17 @@ public class BookController {
 		return "editbook";
 	}
 	
+	// RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Book findBookRest(@PathVariable("id") Long id) {	
+    	return repository.findOne(id);
+    }       
+	
 	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long id, Model model){
 		repository.delete(id);
 		return "redirect:../booklist";
 	}
 }
+
+
